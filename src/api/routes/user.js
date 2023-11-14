d9c45546cf823d985bbfc3a4f7abf88f045132ea
TuanAnh1003 <anhaanh2003@gmail.com>
2023-11-14 12:04:57 +0700

    fix: search user error provide token

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import { addUser, searchUser } from '../controllers/user/index.js';
-
+import { auth } from '../middlewares/index.js';
 const router = Router();
 
 router.post('', addUser);
-router.get('', searchUser);
+router.get('', auth, searchUser);
 export default router
